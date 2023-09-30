64e47976cc80ce7d3e9f122c87f209c41e77c901
Leon Si <leon@leondreamed.com>
2023-09-30 17:19:35 -0400

    fix: tests

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-'use strict';
-const fileType = require('file-type');
-const seekBzip = require('seek-bzip');
+import fileType from "file-type";
+import seekBzip from "seek-bzip";
 
-module.exports = opts => input => {
-	opts = opts || {};
+export default (opts) => (input) => {
+  opts = opts || {};
 
-	if (!Buffer.isBuffer(input)) {
-		return Promise.reject(new TypeError(`Expected a Buffer, got ${typeof input}`));
-	}
+  if (!Buffer.isBuffer(input)) {
+    return Promise.reject(
+      new TypeError(`Expected a Buffer, got ${typeof input}`)
+    );
+  }
 
-	if (!fileType(input) || fileType(input).ext !== 'bz2') {
-		return Promise.resolve([]);
-	}
+  if (fileType(input)?.ext !== "bz2") {
+    return Promise.resolve([]);
+  }
 
-	return new Promise((resolve, reject) => {
-		try {
-			const data = seekBzip.decode(input);
+  return new Promise((resolve, reject) => {
+    try {
+      const data = seekBzip.decode(input);
 
-			resolve([{
-				data,
-				path: opts.path
-			}]);
-		} catch (err) {
-			reject(err);
-		}
-	});
+      resolve([
+        {
+          data,
+          path: opts.path,
+        },
+      ]);
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
--- test.js
+++ test.js
@@ -1,20 +1,22 @@
-import fs from 'fs';
-import path from 'path';
-import isJpg from 'is-jpg';
-import pify from 'pify';
-import test from 'ava';
-import m from './';
+import fs from "fs";
+import path from "path";
+import isJpg from "is-jpg";
+import test from "ava";
+import m from "./index.js";
+import { join } from "desm";
 
-const fsP = pify(fs);
+test("extract file", async (t) => {
+  const buf = await fs.promises.readFile(
+    join(import.meta.url, "fixture.jpg.bz2")
+  );
+  const files = await m({ path: "test.jpg" })(buf);
 
-test('extract file', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixture.jpg.bz2'));
-	const files = await m({path: 'test.jpg'})(buf);
-
-	t.is(files[0].path, 'test.jpg');
-	t.true(isJpg(files[0].data));
+  t.is(files[0].path, "test.jpg");
+  t.true(isJpg(files[0].data));
 });
 
-test('throw on wrong input', async t => {
-	await t.throws(m()('foo'), 'Expected a Buffer, got string');
+test("throw on wrong input", async (t) => {
+  await t.throwsAsync(() => m()("foo"), {
+    message: "Expected a Buffer, got string",
+  });
 });
